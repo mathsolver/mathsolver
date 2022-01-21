a59eee8ad12c910a348d68a5fdbfc64d86424f6d
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-01-21 16:11:34 +0100

    It can remove brackets with plus

---
+++ src/Simplify/RemoveBracketsWithPlus.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace MathSolver\Simplify;
+
+use Illuminate\Support\Collection;
+use MathSolver\Utilities\Node;
+
+class RemoveBracketsWithPlus
+{
+    /**
+     * The current node to remove the brackets on.
+     */
+    public Node $node;
+
+    /**
+     * Remove brackets with a plus. For example 3(p + 4) -> 3p + 3*4 and (x+3)(y-5) -> xy + 5x + 3y + 3*-5.
+     */
+    public function run(Node $node): Node
+    {
+        $node->setChildren($node->children()->map(fn ($child) => $this->run($child)));
+        $this->node = $node;
+
+        if (!$this->shouldRun()) {
+            $this->removeNestedOperations();
+            return $this->node;
+        }
+
+        [$children1, $children2] = $this->getChildrenToMultiply();
+        $this->multiplyChildren($children1, $children2);
+
+        if ($this->node->children()->count() === 1 && $this->node->children()->first()->value() === '(') {
+            $this->node = $this->node->children()->first()->children()->first();
+            $this->node->setParent(null);
+        }
+
+        return $this->run($this->node);
+    }
+
+    /**
+     * Determine whether this check should be run.
+     */
+    protected function shouldRun(): bool
+    {
+        if ($this->node->value() !== '*') {
+            return false;
+        }
+
+        if ($this->node->children()->count() <= 1) {
+            return false;
+        }
+
+        return $this->node->children()->filter(function (Node $brackets) {
+            return $brackets->value() === '(' && $brackets->children()->first()->value() === '+';
+        })->count() > 0;
+    }
+
+    /**
+     * Get two collections of nodes to multiply with eachother.
+     *
+     * @return Collection[]
+     */
+    protected function getChildrenToMultiply(): array
+    {
+        $brackets = $this->node->children()->filter(function (Node $brackets) {
+            return $brackets->value() === '(' && $brackets->children()->first()->value() === '+';
+        })->first();
+
+        $this->node->removeChild($brackets);
+        $bracketChildren = $brackets->children()->first()->children();
+
+        $otherNode = $this->node->children()->filter(fn ($child) => $child !== $brackets)->first();
+        $this->node->removeChild($otherNode);
+
+        $otherChildren = $otherNode->value() === '(' && $otherNode->children()->first()->value() === '+'
+            ? $otherNode->children()->first()->children()
+            : collect([$otherNode]);
+
+        return [$bracketChildren, $otherChildren];
+    }
+
+    /**
+     * Append all the children as a multiplication.
+     */
+    protected function multiplyChildren(Collection $children1, Collection $children2): void
+    {
+        $brackets = $this->node->appendChild(new Node('('));
+        $plus = $brackets->appendChild(new Node('+'));
+
+        $children1->each(function (Node $child1) use ($plus, $children2) {
+            $children2->each(function (Node $child2) use ($plus, $child1) {
+                $times = $plus->appendChild(new Node('*'));
+
+                $child2->value() === '*'
+                    ? $child2->children()->each(fn ($child) => $times->appendChild(clone $child))
+                    : $times->appendChild(clone $child2);
+
+                $child1->value() === '*'
+                    ? $child1->children()->each(fn ($child) => $times->appendChild(clone $child))
+                    : $times->appendChild(clone $child1);
+            });
+        });
+    }
+
+    /**
+     * If a operation contains other same-operation-nodes, remove them.
+     */
+    protected function removeNestedOperations(): void
+    {
+        if (!in_array($this->node->value(), ['+', '*'])) {
+            return;
+        }
+
+        $this->node
+            ->children()
+            ->filter(fn ($child) => $child->value() === $this->node->value())
+            ->each(function ($plus) {
+                $this->node->removeChild($plus);
+                $plus->children()->each(fn ($child) => $this->node->appendChild($child));
+                $this->node->setChildren($this->node->children()->values());
+            });
+    }
+}
---
+++ tests/Simplify/RemoveBracketsWithPlusTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use MathSolver\Simplify\RemoveBracketsWithPlus;
+use MathSolver\Utilities\StringToTreeConverter;
+
+it('removes single brackets', function () {
+    $tree = StringToTreeConverter::run('3(p + 5)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('3p + 3*5'));
+
+    $tree = StringToTreeConverter::run('5(a + b)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('5a + 5b'));
+});
+
+it('removes double brackets', function () {
+    $tree = StringToTreeConverter::run('(x + 3)(x - 5)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('x*x - 5x + x*3 - 5*3'));
+
+    $tree = StringToTreeConverter::run('5(a + 7)(b - 9)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('5ab + 5*7*b + 5*a*-9 + 5*7*-9'));
+});
+
+it('removes with exponents', function () {
+    $tree = StringToTreeConverter::run('5(x^2 + y^5)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('5x^2 + 5y^5'));
+});
+
+it('removes with more than two sets of brackets', function () {
+    $tree = StringToTreeConverter::run('(x + 3)(x - 5)(x - 9)(4 + x)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('4*x*x*x + x*x*x*x + 4*-9*x*x + x*-9*x*x + 4*x*-5*x + x*x*-5*x + 4*-9*-5*x + x*-9*-5*x + 4*x*x*3 + x*x*x*3 + 4*-9*x*3 + x*-9*x*3 + 4*x*-5*3 + x*x*-5*3 + 4*-9*-5*3 + x*-9*-5*3'));
+
+    $tree = StringToTreeConverter::run('(a + 3)(b - 5)(c + 3)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('b*a*c + -5*a*c + b*3*c + -5*3*c + b*a*3 + -5*a*3 + b*3*3 + -5*3*3'));
+});
+
+it('removes with more than two terms inside the brackets', function () {
+    $tree = StringToTreeConverter::run('(x + y + 9)(x + y)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('x*x + yx + xy + y*y + x*9 + y*9'));
+
+    $tree = StringToTreeConverter::run('(a + b + c + d)(e + f + g + h)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('ea + fa + ga + ha + eb + fb + gb + hb + ec + fc + gc + hc + ed + fd + gd + hd'));
+});
+
+it('removes when nested', function () {
+    $tree = StringToTreeConverter::run('6(a + 2b) + 2(a + 3b)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('6a + 6*2*b + 2a + 2*3*b'));
+
+    $tree = StringToTreeConverter::run('-4x - 5(7x - 2y)');
+    $result = (new RemoveBracketsWithPlus())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('-4x + -5*7*x + -5*-2*y'));
+});
