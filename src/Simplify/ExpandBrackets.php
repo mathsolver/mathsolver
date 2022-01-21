78a04de8db293d74d51a80f27f40c050fbfc9676
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-01-21 16:03:17 +0100

    It can expand brackets

---
+++ src/Simplify/ExpandBrackets.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace MathSolver\Simplify;
+
+use MathSolver\Utilities\Node;
+
+class ExpandBrackets
+{
+    /**
+     * Expand all brackets with an exponent.
+     *
+     * For example: (x + 2)^3 -> (x + 2)(x + 2)(x + 2).
+     */
+    public function run(Node $node): Node
+    {
+        $node->setChildren($node->children()->map(fn ($child) => $this->run($child)));
+
+        if (!$this->shouldRun($node)) {
+            return $this->removeDoubleTimes($node);
+        }
+
+        $times = new Node('*');
+
+        for ($x = 1; $x <= $node->children()->last()->value(); $x++) {
+            $times->appendChild(tap(unserialize(serialize($node->children()->first())))->setParent($times));
+        }
+
+        return $times;
+    }
+
+    /**
+     * Determine whether the function should run.
+     */
+    protected function shouldRun(Node $node): bool
+    {
+        if ($node->value() !== '^') {
+            return false;
+        }
+
+        if ($node->children()->first()->value() !== '(') {
+            return false;
+        }
+
+        return !(!is_numeric($node->children()->last()->value()));
+    }
+
+    /**
+     * Remove a nested times in the math tree.
+     */
+    protected function removeDoubleTimes(Node $node): Node
+    {
+        if ($node->value() !== '*') {
+            return $node;
+        }
+
+        $nestedTimeses = $node->children()->filter(fn ($child) => $child->value() === '*');
+
+        $nestedTimeses->each(function ($nestedTimes) use ($node) {
+            $nestedTimes->children()->each(function ($nestedTimesChild) use ($node) {
+                $node->appendChild(tap(unserialize(serialize($nestedTimesChild)))->setParent($node));
+            });
+
+            $node->removeChild($nestedTimes);
+        });
+
+        $node->setChildren($node->children()->values());
+
+        return $node;
+    }
+}
---
+++ tests/Simplify/ExpandBracketsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use MathSolver\Simplify\ExpandBrackets;
+use MathSolver\Utilities\StringToTreeConverter;
+
+it('can expand brackets', function () {
+    $tree = StringToTreeConverter::run('(x + 3)^2');
+    $result = (new ExpandBrackets())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('(x + 3)(x + 3)'));
+});
+
+it('does not expand brackets when there is no numeric exponent', function () {
+    $tree = StringToTreeConverter::run('(x + 3)^x');
+    $result = (new ExpandBrackets())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('(x + 3)^x'));
+});
+
+it('expands with brackets with more terms', function () {
+    $tree = StringToTreeConverter::run('(x + 3 - 9)^4');
+    $result = (new ExpandBrackets())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('(x + 3 - 9)(x + 3 - 9)(x + 3 - 9)(x + 3 - 9)'));
+});
+
+it('expands with brackets with one term', function () {
+    $tree = StringToTreeConverter::run('(y)^6');
+    $result = (new ExpandBrackets())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('(y)(y)(y)(y)(y)(y)'));
+});
+
+it('expands with a leading minus', function () {
+    $tree = StringToTreeConverter::run('-1 * (x + 8)^2');
+    $result = (new ExpandBrackets())->run($tree);
+    expect($result)->toEqual(StringToTreeConverter::run('-(x + 8)(x + 8)'));
+});
