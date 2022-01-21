2fbe9441789b96b9fa0371110b1dd5eb1705734f
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-01-21 16:10:44 +0100

    It can multiply real numbers

---
+++ src/Simplify/MultiplyRealNumbers.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MathSolver\Simplify;
+
+use MathSolver\Utilities\Node;
+
+class MultiplyRealNumbers
+{
+    /**
+     * Multiply all real numbers.
+     *
+     * For example 2 * 3 * 6 -> 36, 2 * 4 * x -> 8 * x.
+     */
+    public function run(Node $node): Node
+    {
+        $node->setChildren($node->children()->map(fn ($child) => $this->run($child)));
+
+        if ($node->value() !== '*') {
+            return $node;
+        }
+
+        $total = $node->numericChildren()
+            ->each(fn ($child) => $node->removeChild($child))
+            ->reduce(fn ($total, $number) => $number->value() * $total, 1);
+
+        if ($node->nonNumericChildren()->count() === 0) {
+            return new Node($total);
+        }
+
+        if ($total !== 1) {
+            $node->appendChild(new Node($total), $top = true);
+        }
+
+        return tap($node)->setChildren($node->children()->sortBy(fn ($child) => $child->value(), SORT_NATURAL)->values());
+    }
+}
