693953af3936e33ff058c59e7d889058f192bf45
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-01-25 15:39:16 +0100

    Add comments to `Step`

--- src/Simplify/Step.php
+++ src/Simplify/Step.php
@@ -24,14 +24,17 @@ abstract class Step
      *
      * @return Collection<Node>|Node
      */
-    public static function run(Node $node): Node|Collection
+    public static function run(Node $node): Collection|Node
     {
+        // Run this function resursively for all children
         $node->setChildren($node->children()->map(fn ($child) => self::run($child))->flatten());
 
+        // Determine whether this function should run for this node
         if (!(new (get_called_class()))->shouldRun($node)) {
             return $node;
         }
 
+        // Execute the function
         return (new (get_called_class()))->handle($node);
     }
 }
