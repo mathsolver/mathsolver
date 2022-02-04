975e70cef7e2702a5fa89ceb164b7057c4b78c52
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-02-04 13:30:03 +0100

    Add tests

--- tests/Solve/SolveTest.php
+++ tests/Solve/SolveTest.php
@@ -3,9 +3,9 @@
 use MathSolver\Solve\Solver;
 use MathSolver\Utilities\StringToTreeConverter;
 
-it('can solve linear equations by subtracting', function (string $input, string $expected) {
+it('can solve linear equations', function (string $input, string $expected, string $solveFor = 'x') {
     $tree = StringToTreeConverter::run($input);
-    $result = Solver::run($tree, 'x');
+    $result = Solver::run($tree, $solveFor);
     $expected = StringToTreeConverter::run($expected);
     expect($result)->toEqual($expected);
 })->with([
@@ -19,28 +19,17 @@ it('can solve linear equations by subtracting', function (string $input, string 
     ['x + 7 = 10', '3'],
     ['x + 1 = 0', '-1'],
     ['9 + x = 11', '2'],
-]);
-
-it('can solve linear equations by dividing', function (string $input, string $expected) {
-    $tree = StringToTreeConverter::run($input);
-    $result = Solver::run($tree, 'x');
-    $expected = StringToTreeConverter::run($expected);
-    expect($result)->toEqual($expected);
-})->with([
     ['5x = 25', '5'],
     ['3x = 27', '9'],
     ['8x = 40', '5'],
     ['28x = 1316', '47'],
     ['-2x = 4', '-2'],
-]);
-
-it('can solve linear equations by substracting and dividing', function (string $input, string $expected) {
-    $tree = StringToTreeConverter::run($input);
-    $result = Solver::run($tree, 'x');
-    $expected = StringToTreeConverter::run($expected);
-    expect($result)->toEqual($expected);
-})->with([
     ['2x + 5 = 15', '5'],
     ['-5x + 8 = 53', '-9'],
     ['6 - x = 4', '2'],
+    ['10x - 12 = -12', '0'],
+    ['-16x = -4', 'frac(1, 4)'],
+    ['14x + 3 = 2', 'frac(-1, 14)'],
+    ['-6 - a = -4', '-2', 'a'],
+    ['frac(1, 2) - 4a = frac(9, 2)', '-1', 'a'],
 ]);
