127d28176ef130c0a89a5ae085dde940f36ceb01
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-02-04 14:16:18 +0100

    Fix failing test

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -56,5 +56,5 @@ it('records steps for substitution', function () {
 });
 
 it('can solve equations', function () {
-    expect(Math::from('2x = 16')->solveFor('x'))->toBe('8');
+    expect(Math::from('2x = 16')->solveFor('x')->mathjax())->toBe('8');
 });
