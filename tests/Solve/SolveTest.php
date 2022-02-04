77f9b1fea328df3e980485247ad6dd096c157844
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-02-04 10:47:59 +0100

    Add tests

--- tests/Solve/SolveTest.php
+++ tests/Solve/SolveTest.php
@@ -3,7 +3,7 @@
 use MathSolver\Solve\Solver;
 use MathSolver\Utilities\StringToTreeConverter;
 
-it('can solve simple linear equations', function (string $input, string $expected) {
+it('can solve linear equations by adding', function (string $input, string $expected) {
     $tree = StringToTreeConverter::run($input);
     $result = Solver::run($tree, 'x');
     $expected = StringToTreeConverter::run($expected);
@@ -15,4 +15,7 @@ it('can solve simple linear equations', function (string $input, string $expecte
     ['x + 4 = -8', '-12'],
     ['5 + x = 8', '3'],
     ['x = 6', '6'],
+    ['x + 3 = y + 4', 'y + 1'],
+    ['x + 7 = 10', '3'],
+    ['x + 1 = 0', '-1'],
 ]);
