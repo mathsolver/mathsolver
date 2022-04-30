eecbffbab77ce60807357c8818b60e1d6c4a64b7
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-04-30 12:19:30 +0300

    Add test

--- tests/Derivatives/DerivativeTest.php
+++ tests/Derivatives/DerivativeTest.php
@@ -11,4 +11,5 @@ it('can differentiate functions', function (string $input, string $expected) {
 })->with([
     ['5x^6 - 3x^5 + 2x - 7', '30x^5 - 15x^4 + 2'],
     ['-2x^8 - 4x^4 + 7.2', '-16x^7 - 16x^3'],
+    ['-frac(1, 3)x^3 - frac(1, 2)x^2 - x - 1', '-x^2 - x - 1'],
 ]);
