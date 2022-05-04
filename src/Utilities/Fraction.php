456919adaaf27fda72b66921bef3a5dbb2fca2e7
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-05-04 10:02:18 +0300

    Create a `Fraction` class

---
+++ src/Utilities/Fraction.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace MathSolver\Utilities;
+
+class Fraction
+{
+    /**
+     * The numerator.
+     */
+    protected int $numerator;
+
+    /**
+     * The denominator.
+     */
+    protected int $denominator;
+
+    /**
+     * Instantiate a new fraction.
+     *
+     * @return self
+     */
+    public function __construct(int $numerator, int $denominator)
+    {
+        $this->numerator = $numerator;
+        $this->denominator = $denominator;
+    }
+
+    /**
+     * Get the numerator.
+     */
+    public function numerator(): int
+    {
+        return $this->numerator;
+    }
+
+    /**
+     * Get the denominator.
+     */
+    public function denominator(): int
+    {
+        return $this->denominator;
+    }
+
+    /**
+     * Convert this fraction to a node.
+     */
+    public function node(): Node
+    {
+        if ($this->denominator === 1) {
+            return new Node($this->numerator);
+        }
+
+        $node = new Node('frac');
+        $node->appendChild(new Node($this->numerator));
+        $node->appendChild(new Node($this->denominator));
+
+        return $node;
+    }
+
+    /**
+     * Simplify the fraction by dividing both the
+     * numerator and denominator by the same value.
+     *
+     * For example: 2/4 => 1/2 and -3/-9 => 1/3.
+     */
+    public function simplify(): self
+    {
+        $greatestCommonDivisor = (int) gmp_gcd($this->numerator, $this->denominator);
+
+        if ($this->numerator < 0 && $this->denominator < 0) {
+            $greatestCommonDivisor *= -1;
+        }
+
+        $this->numerator /= $greatestCommonDivisor;
+        $this->denominator /= $greatestCommonDivisor;
+
+        return $this;
+    }
+
+    /**
+     * Multiply this fraction by another fraction or a whole number.
+     */
+    public function multiply(int $numerator, int $denominator = 1): self
+    {
+        $this->numerator *= $numerator;
+        $this->denominator *= $denominator;
+        return $this;
+    }
+
+    /**
+     * Divide this fraction by another fraction or a whole number.
+     *
+     * Math rule: keep-change-flip. This means that in order to
+     * divide by a fraction, you multiply by the reprocipical.
+     * For example 1/2 / 3/4 => 1/2 * 4/3.
+     */
+    public function divide(int $numerator, int $denominator = 1): self
+    {
+        // Math rule: keep-change-flip
+        return $this->multiply($denominator, $numerator);
+    }
+
+    /**
+     * Add this fraction by another fraction or a whole number.
+     *
+     * In order to add fractions, you need the same denominators.
+     * If the denominators aren't the same, you can use the
+     * formula A/B + C/D = (AD + BC)/(BD).
+     */
+    public function add(int $numerator, int $denominator = 1): self
+    {
+        if ($denominator === $this->denominator) {
+            $this->numerator += $numerator;
+            return $this;
+        }
+
+        $this->numerator = ($this->numerator * $denominator) + ($numerator * $this->denominator);
+        $this->denominator = $this->denominator * $denominator;
+
+        return $this;
+    }
+
+    /**
+     * Subtract this fraction by another fraction or a whole number.
+     *
+     * To subract, multiply the numerator of one of the fractions
+     * by -1, and then add them up.
+     */
+    public function subtract(int $numerator, int $denominator): self
+    {
+        return $this->add($numerator * -1, $denominator);
+    }
+}
---
+++ tests/Utilities/FractionTest.php
@@ -0,0 +1,97 @@
+<?php
+
+use MathSolver\Utilities\Fraction;
+use MathSolver\Utilities\Node;
+
+it('can instantiate a fraction', function () {
+    $fraction = new Fraction(2, 3);
+    expect($fraction->numerator())->toBe(2);
+    expect($fraction->denominator())->toBe(3);
+});
+
+it('can convert to a node', function () {
+    $node = new Node('frac');
+    $node->appendChild(new Node(2));
+    $node->appendChild(new Node(3));
+
+    $fraction = new Fraction(2, 3);
+    expect($fraction->node())->toEqual($node);
+});
+
+it('can convert to a node without a fraction', function () {
+    $node = new Node(5);
+    $fraction = new Fraction(5, 1);
+    expect($fraction->node())->toEqual($node);
+});
+
+it('can simplify fractions', function () {
+    $fraction = new Fraction(2, 4);
+    $fraction->simplify();
+    expect($fraction->numerator())->toBe(1)->and($fraction->denominator())->toBe(2);
+});
+
+it('removes minus in the numerator and the denominator', function () {
+    $fraction = new Fraction(-1, -3);
+    $fraction->simplify();
+    expect($fraction->numerator())->toBe(1)->and($fraction->denominator())->toBe(3);
+});
+
+test('sometimes it cant be simplified', function () {
+    $fraction = new Fraction(3, 4);
+    $fraction->simplify();
+    expect($fraction->numerator())->toBe(3)->and($fraction->denominator())->toBe(4);
+});
+
+it('can multiply fractions', function () {
+    $fraction = new Fraction(2, 3);
+    $fraction->multiply(3, 4);
+    expect($fraction->numerator())->toBe(6)->and($fraction->denominator())->toBe(12);
+});
+
+it('can multiply by a whole number', function () {
+    $fraction = new Fraction(2, 3);
+    $fraction->divide(1, 2);
+    expect($fraction->numerator())->toBe(4)->and($fraction->denominator())->toBe(3);
+});
+
+it('can divide fractions', function () {
+    $fraction = new Fraction(2, 3);
+    $fraction->multiply(3, 4);
+    expect($fraction->numerator())->toBe(6)->and($fraction->denominator())->toBe(12);
+});
+
+it('can divide by a whole number', function () {
+    $fraction = new Fraction(3, 4);
+    $fraction->divide(2);
+    expect($fraction->numerator())->toBe(3)->and($fraction->denominator())->toBe(8);
+});
+
+it('can add fractions', function () {
+    $fraction = new Fraction(1, 2);
+    $fraction->add(3, 2);
+    expect($fraction->numerator())->toBe(4)->and($fraction->denominator())->toBe(2);
+});
+
+it('can add with different denominators', function () {
+    $fraction = new Fraction(2, 3);
+    $fraction->add(3, 4);
+    expect($fraction->numerator())->toBe(17)->and($fraction->denominator())->toBe(12);
+});
+
+it('can add whole numbers', function () {
+    $fraction = new Fraction(3, 7);
+    $fraction->add(2);
+    expect($fraction->numerator())->toBe(17)->and($fraction->denominator())->toBe(7);
+});
+
+it('can subtract fractions', function () {
+    $fraction = new Fraction(2, 3);
+    $fraction->subtract(1, 3);
+    expect($fraction->numerator())->toBe(1)->and($fraction->denominator())->toBe(3);
+});
+
+it('can subtract fractions with different denominators', function () {
+    $fraction = new Fraction(4, 7);
+    $fraction->subtract(1, 8);
+    expect($fraction->numerator())->toBe(25)->and($fraction->denominator())->toBe(56);
+});
