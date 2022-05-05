0c29ed07b492b14e1dda2f8638b66a20627f845c
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-05-05 19:30:46 +0300

    Refactor

--- tests/Utilities/FractionTest.php
+++ tests/Utilities/FractionTest.php
@@ -32,62 +32,62 @@ it('can convert to a node without a fraction', function () {
 
 it('can simplify fractions', function () {
     $fraction = (new Fraction(2, 4))->simplify();
-    expect($fraction->numerator())->toBe(1)->and($fraction->denominator())->toBe(2);
+    expect($fraction)->toEqual(new Fraction(1, 2));
 });
 
 it('removes minus in the numerator and the denominator', function () {
     $fraction = (new Fraction(-1, -3))->simplify();
-    expect($fraction->numerator())->toBe(1)->and($fraction->denominator())->toBe(3);
+    expect($fraction)->toEqual(new Fraction(1, 3));
 });
 
 test('sometimes it cant be simplified', function () {
     $fraction = (new Fraction(3, 4))->simplify();
-    expect($fraction->numerator())->toBe(3)->and($fraction->denominator())->toBe(4);
+    expect($fraction)->toEqual(new Fraction(3, 4));
 });
 
 it('can multiply fractions', function () {
     $fraction = (new Fraction(2, 3))->multiply(3, 4);
-    expect($fraction->numerator())->toBe(6)->and($fraction->denominator())->toBe(12);
+    expect($fraction)->toEqual(new Fraction(6, 12));
 });
 
 it('can multiply by a whole number', function () {
     $fraction = (new Fraction(2, 3))->multiply(2);
-    expect($fraction->numerator())->toBe(4)->and($fraction->denominator())->toBe(3);
+    expect($fraction)->toEqual(new Fraction(4, 3));
 });
 
 it('can divide fractions', function () {
     $fraction = (new Fraction(2, 3))->divide(4, 3);
-    expect($fraction->numerator())->toBe(6)->and($fraction->denominator())->toBe(12);
+    expect($fraction)->toEqual(new Fraction(6, 12));
 });
 
 it('can divide by a whole number', function () {
     $fraction = (new Fraction(3, 4))->divide(2);
-    expect($fraction->numerator())->toBe(3)->and($fraction->denominator())->toBe(8);
+    expect($fraction)->toEqual(new Fraction(3, 8));
 });
 
 it('can add fractions', function () {
     $fraction = (new Fraction(1, 2))->add(3, 2);
-    expect($fraction->numerator())->toBe(4)->and($fraction->denominator())->toBe(2);
+    expect($fraction)->toEqual(new Fraction(4, 2));
 });
 
 it('can add with different denominators', function () {
     $fraction = (new Fraction(2, 3))->add(3, 4);
-    expect($fraction->numerator())->toBe(17)->and($fraction->denominator())->toBe(12);
+    expect($fraction)->toEqual(new Fraction(17, 12));
 });
 
 it('can add whole numbers', function () {
     $fraction = (new Fraction(3, 7))->add(2);
-    expect($fraction->numerator())->toBe(17)->and($fraction->denominator())->toBe(7);
+    expect($fraction)->toEqual(new Fraction(17, 7));
 });
 
 it('can subtract fractions', function () {
     $fraction = (new Fraction(2, 3))->subtract(1, 3);
-    expect($fraction->numerator())->toBe(1)->and($fraction->denominator())->toBe(3);
+    expect($fraction)->toEqual(new Fraction(1, 3));
 });
 
 it('can subtract fractions with different denominators', function () {
     $fraction = (new Fraction(4, 7))->subtract(1, 8);
-    expect($fraction->numerator())->toBe(25)->and($fraction->denominator())->toBe(56);
+    expect($fraction)->toEqual(new Fraction(25, 56));
 });
 
 it('can get the whole part', function (int $numerator, int $denominator, int $wholePart) {
