a56e7f7129144041bff41654fc8b50199c4ce0ef
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-05-05 11:26:36 +0300

    Rename `SimplifyTest` to `RunnerTest`

--- tests/RunnerTest.php
+++ tests/RunnerTest.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file contains a lot of math expressions, to
+ * test that all the steps can work together.
+ */
+
 use MathSolver\Runner;
 use MathSolver\Utilities\StringToTreeConverter;
 
@@ -220,14 +225,28 @@ it('can calculate functions', function (string $input, string $expected) {
     ['log(4, 16)', 'frac(1, 2)'],
 ]);
 
-it('can differentiate', function (string $input, string $expected) {
-    $tree = StringToTreeConverter::run($input);
+it('can differentiate', function (string $input, string $expected, string $respect = 'x') {
+    $tree = $respect == 'x' ? StringToTreeConverter::run("deriv({$input})") : StringToTreeConverter::run("deriv({$input}, {$respect})");
     $result = Runner::run($tree)['result'];
     $expected = StringToTreeConverter::run($expected);
     expect($result)->toEqual($expected);
 })->with([
-    ['deriv(x^2)', '2x'],
-    ['deriv(x^-1)', '-x^-2'],
+    ['x^2', '2x'],
+    ['x^-1', '-x^-2'],
+    ['5x^6 - 3x^5 + 2x - 7', '30x^5 - 15x^4 + 2'],
+    ['-2x^8 - 4x^4 + 7.2', '-16x^7 - 16x^3'],
+    ['-frac(1, 3)x^3 - frac(1, 2)x^2 - x - 1', '-x^2 - x - 1'],
+    ['1 + 3q - 3q^2 - 5q^7', '-35q^6 - 6q + 3', 'q'],
+    ['(5x + 7)(4 - 3x)', '-30x - 1'],
+    ['(3x + 6)^2 - 8x', '18x + 28'],
+    ['5(x - 3)^2 + 5(2x - 1)', '10x - 20'],
+    ['-3(x - 1)(5 - 9x) - 8(x - 7)', '54x - 50'],
+    ['(3x - 1)(x^2 + 5x)', '9x^2 + 28x - 5'],
+    ['(3x^3 - 1)^2', '54x^5 - 18x^2'],
+    ['(5x^5 - 3)(3x - 2)', '90x^5 - 50x^4 - 9'],
+    ['5 - 3(x^4 - x)(x + 1)', '-15x^4 - 12x^3 + 6x + 3'],
+    ['(5t^3 - t)(3t^5 + t)', '120t^7 + 20t^3 - 18t^5 - 2t', 't'],
+    ['1 - (3q^2 - 2)^2', '-36q^3 + 24q', 'q'],
 ]);
 
 it('records steps', function () {
